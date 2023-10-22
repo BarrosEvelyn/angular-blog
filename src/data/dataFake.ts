24c05be7811e57003a4a49c99f8521565b40095f
BarrosEvelyn <evelyn_barros1@hotmail.com>
2023-10-22 00:33:07 -0300

    content criado e projeto feito

---
+++ src/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+   {
+    "id":"1",
+    "title": "Um pouco sobre mim",
+    "description": "Sou formada em Administração de Empresas, e iniciei meu estudo em Análise e Desenvolvimento de Sistemas. Estou entusiasmada com a perspectiva de aplicar meus conhecimentos em programação e tecnologia para agregar valor em novos desafios e oportunidades que surgirão na minha carreira.",
+    "photoCover":"assets/images/mulhersorrindo.jpg"
+   },
+   {
+   "id":"2",
+    "title": "Hobby",
+    "description": "Na melhor que ter um momento de descanso e lazer para descansar o corpo e a mente. Por esse motivo, valorizo muito ter hobbies. Os meus são ler livros, viajar e passear em parque com a família.",
+    "photoCover":"assets/images/lifestyle.jpg"
+   },
+   {
+   "id":"3",
+    "title": "Ampliando os horizontes",
+    "description": "Iniciei meu projeto de transição de carreira no início de 2023. Todos esses meses venho me dedicando a aprender as linguagens de programação. E, com certeza, foi a melhor escolha que eu poderia ter feito.",
+    "photoCover":"assets/images/estudante.png"
+   },
+   {
+   "id":"4",
+    "title": "Objetivos",
+    "description": "A verdade é que todos nós somos motivados por sonhos e projetos. E eu tenho muitos! Por esse motivo, me dedico ao máximo e busco as melhores alternativas em todos os aspectos da minha vida.",
+    "photoCover":"assets/images/sonhos.jpg"
+   }    
+]
